75ba241f117112c4dfd0def2bc6160ffd47e60f1
Danielateresa <dany.t_87@hotmail.it>
2023-01-14 12:20:30 +0100

    Create index, crud routes and public to img

--- database/seeders/FruitSeeder.php
+++ database/seeders/FruitSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+use App\Models\Fruit;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class FruitSeeder extends Seeder
 {
@@ -12,8 +15,16 @@ class FruitSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i <10 ; $i++) { 
+            $fruit = new Fruit();
+            $fruit->img = 'placeholders/' . $faker->image('storage/app/public/placeholders',400, 200, 'Project', false, false);
+            $fruit->name = $faker->sentence(3);
+            $fruit->slug = Str::slug($fruit->name,'-');
+            $fruit->weight = $faker->randomFloat(2, 50, 700);
+            $fruit->price = $faker->randomFloat(2, 1, 20);
+            $fruit->save();
+        }
     }
 }
